83766b53af016877649c91d4977b09755f05ec08
Vinícius Arruda <viniarruda@hdu15332.gabril.com.br>
2017-12-08 16:19:41 -0200

    Finish Search TodoList

--- README.md
+++ README.md
@@ -45,3 +45,8 @@
 	- npm init -y
 	- npm i --save-dev body-parser@1.15.2 express@4.14.0 mongoose@4.7.0 node-restful@0.2.5 pm2@2.1.5 nodemon@1.11.0
 	- npm run dev
+
+
+# Rodando todolist
+	- mongod
+	- npm run dev back / front
--- todolist-app/frontend/src/todo/todoForm.jsx
+++ todolist-app/frontend/src/todo/todoForm.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
+            <input id='description' className='form-control' placeholder='Aula 51' value={props.description} onChange={props.handleChange}></input>
         </Grid>
         <Grid cols='12 3 2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
-            </button>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+            <IconButton style='info' icon='search' onClick={props.handleSearch}></IconButton>
         </Grid>
     </div>
 )
